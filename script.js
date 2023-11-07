bef2341f87ec07da6b74a9cf0f444cb9bc1c2085
Naveen Kumar <nkkaran943@gmail.com>
2023-11-07 11:44:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-  function showTime() {
-  var updateClock = function () {
-    var date = new Date();
-    var h = date.getHours() % 12 || 12;
-    var m = date.getMinutes();
-    var s = date.getSeconds();
-    var session = h < 12 ? "AM" : "PM";
-    
-    var time = h + ":" + (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s + " " + session;
+function showTime() {
+    var updateClock = function () {
+        var date = new Date();
+        var h = date.getHours() % 12 || 12;
+        var m = date.getMinutes();
+        var s = date.getSeconds();
+        var session = h < 12 ? "AM" : "PM";
 
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-  };
+        var time = h + ":" + (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s + " " + session;
 
-  updateClock();
+        document.getElementById("MyClockDisplay").innerText = time;
+        document.getElementById("MyClockDisplay").textContent = time;
 
-  setInterval(updateClock, 1000);
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        var today = date.toLocaleDateString('en-US', options);
+        document.getElementById("DateDisplay").innerText = today;
+        document.getElementById("DateDisplay").textContent = today;
+    };
+
+    updateClock();
+
+    setInterval(updateClock, 1000);
 }
 
 showTime();
